030ec884652549accd1cb68db65136bad036d9fb
GustawB <gustaw.blachowski@gmail.com>
2025-12-05 09:51:42 +0000

    fix: merge_operator not working when there is no base val

--- merge_operator.cc
+++ merge_operator.cc
@@ -1,38 +1,58 @@
 #include "merge_operator.h"
 #include "google/cloud/internal/big_endian.h"
 #include <rocksdb/env.h>
+#include <algorithm>
+#include <limits>
+#include <optional>
 
 namespace google {
 namespace cloud {
 namespace bigtable {
 namespace emulator {
+namespace {
+template <typename Op>
+bool MergeCell(rocksdb::Slice const* existing_value,
+               rocksdb::Slice const& value,
+               std::string* new_value,
+               rocksdb::Logger* logger,
+               int64_t default_if_missing,
+               Op operation) {
+
+  int64_t existing_int = default_if_missing;
+
+  if (existing_value) {
+    auto maybe_existing = google::cloud::internal::DecodeBigEndian<std::int64_t>(
+        existing_value->ToString());
+    if (!maybe_existing) {
+      rocksdb::Log(logger, "Failed to decode current value during merge");
+      return false;
+    }
+    existing_int = maybe_existing.value();
+  }
+
+  auto maybe_new = google::cloud::internal::DecodeBigEndian<std::int64_t>(
+      value.ToString());
+  if (!maybe_new) {
+    rocksdb::Log(logger, "Failed to decode new value during merge");
+    return false;
+  }
+
+  int64_t result = operation(existing_int, maybe_new.value());
+  *new_value = google::cloud::internal::EncodeBigEndian(result);
+
+  return true;
+}
+
+} // anonymous namespace
 
 bool SumUpdateCellBEInt64::Merge(rocksdb::Slice const& key,
                                  rocksdb::Slice const* existing_value,
                                  rocksdb::Slice const& value,
                                  std::string* new_value,
                                  rocksdb::Logger* logger) const {
-  int64_t existing_value_int = 0;
-  if (existing_value) {
-    auto maybe_existing_value_int =
-        google::cloud::internal::DecodeBigEndian<std::int64_t>(
-            existing_value->ToString());
-    if (!maybe_existing_value_int) {
-      rocksdb::Log(logger, ("Failed to decode current value during merge"));
-      return false;
-    }
-    existing_value_int = maybe_existing_value_int.value();
-  }
-
-  auto new_value_int =
-      google::cloud::internal::DecodeBigEndian<std::int64_t>(value.ToString());
-  if (!new_value_int) {
-    rocksdb::Log(logger, ("Failed to decode new value during merge"));
-    return false;
-  }
-  *new_value = google::cloud::internal::EncodeBigEndian(existing_value_int +
-                                                        new_value_int.value());
-  return true;
+  return MergeCell(existing_value, value, new_value, logger,
+                   0,
+                   std::plus<>());
 }
 
 bool MaxUpdateCellBEInt64::Merge(rocksdb::Slice const& key,
@@ -40,26 +60,9 @@ bool MaxUpdateCellBEInt64::Merge(rocksdb::Slice const& key,
                                  rocksdb::Slice const& value,
                                  std::string* new_value,
                                  rocksdb::Logger* logger) const {
-  auto existing_int = google::cloud::internal::DecodeBigEndian<std::int64_t>(
-      existing_value->ToString());
-  if (!existing_int) {
-    rocksdb::Log(logger, ("Failed to decode current value during merge"));
-    return false;
-  }
-  auto new_int =
-      google::cloud::internal::DecodeBigEndian<std::int64_t>(value.ToString());
-  if (!new_int) {
-    rocksdb::Log(logger, ("Failed to decode new value during merge"));
-    return false;
-  }
-
-  if (existing_int.value() > new_int.value()) {
-    *new_value = existing_value->ToString();
-  } else {
-    *new_value = google::cloud::internal::EncodeBigEndian(new_int.value());
-  }
-
-  return true;
+  return MergeCell(existing_value, value, new_value, logger,
+                   std::numeric_limits<int64_t>::min(),
+                   [](int64_t a, int64_t b) { return std::max(a, b); });
 }
 
 bool MinUpdateCellBEInt64::Merge(rocksdb::Slice const& key,
@@ -67,26 +70,9 @@ bool MinUpdateCellBEInt64::Merge(rocksdb::Slice const& key,
                                  rocksdb::Slice const& value,
                                  std::string* new_value,
                                  rocksdb::Logger* logger) const {
-  auto existing_int = google::cloud::internal::DecodeBigEndian<std::int64_t>(
-      existing_value->ToString());
-  if (!existing_int) {
-    rocksdb::Log(logger, ("Failed to decode current value during merge"));
-    return false;
-  }
-  auto new_int =
-      google::cloud::internal::DecodeBigEndian<std::int64_t>(value.ToString());
-  if (!new_int) {
-    rocksdb::Log(logger, ("Failed to decode new value during merge"));
-    return false;
-  }
-
-  if (existing_int.value() < new_int.value()) {
-    *new_value = existing_value->ToString();
-  } else {
-    *new_value = google::cloud::internal::EncodeBigEndian(new_int.value());
-  }
-
-  return true;
+  return MergeCell(existing_value, value, new_value, logger,
+                   std::numeric_limits<int64_t>::max(),
+                   [](int64_t a, int64_t b) { return std::min(a, b); });
 }
 
 }  // namespace emulator
